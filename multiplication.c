075b8d0623cdedfac00f14937ba97b8d5930efd6
Sinan450 <muhammadsinan1633@gmail.com>
2024-11-26 02:26:08 -0800

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int num;
+   printf("enter the number");
+   scanf("%d",&num);
+   for(int i=1;i<=10;i++)
+   {
+   printf("%dx%d=%d\n",num,i,num*i);
+}
+return 0;
+}
