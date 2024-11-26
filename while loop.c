4e6abaadbc1dfb6b1aade5e163ae5fec90588ac3
Sinan450 <muhammadsinan1633@gmail.com>
2024-11-26 01:27:43 -0800

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n,i=1,sum=0;
+    printf("enter the value for n:");
+     scanf("%d",&n);
+    while(i<=n){
+        sum+=i;
+    i++;
+  }
+  return 0;
+}
